2a85fba1b13e569bda9d8926c2b532ede9f664f1
max <ntchjb@users.noreply.github.com>
2024-08-18 17:56:12 +0700

    fix(sample): always run deferred functions instead of panic

--- sample/eth/main.go
+++ sample/eth/main.go
@@ -19,14 +19,15 @@ func main() {
 
 	defer func() {
 		if err := man.Close(); err != nil {
-			panic(err)
+			logger.Error("unable to close device manager", "err", err)
 		}
 	}()
 
 	// List USB HID devices
 	deviceInfos, err := man.Enumerate(0, 0)
 	if err != nil {
-		panic(err)
+		logger.Error("unable to enumerate devices", "err", err)
+		return
 	}
 
 	logger.Info("Device info", "info", deviceInfos.String())
@@ -36,25 +37,30 @@ func main() {
 		StreamLaneCount: hid.DEFAULT_ENDPOINT_STREAM_COUNT,
 	})
 	if err != nil {
-		panic(err)
+		logger.Error("unable to open device")
+		return
 	}
 	defer hidDevice.Close()
 
 	if err := hidDevice.SetTarget(1, 0, 0); err != nil {
-		panic(err)
+		logger.Error("unable to set target of hid device", "err", err)
+		return
 	}
 
 	// Get report descriptor
 	desc, err := hidDevice.GetReportDescriptor()
 	if err != nil {
-		panic(err)
+		logger.Error("unable to get report descriptor", "err", err)
+		return
 	}
 
 	str, err := desc.String()
 	if err != nil {
-		panic(err)
+		logger.Error("unable to get descriptor string", "err", err)
+		return
 	}
-	logger.Info("Report descriptor", "desc", str)
+	fmt.Printf("%s\n", str)
+	fmt.Printf("%v\n", desc)
 
 	ledgerDevice := device.NewLedgerDevice(hidDevice)
 	adpuProto := adpu.NewProtocol(ledgerDevice, 1234, logger)
@@ -62,12 +68,13 @@ func main() {
 	ctx := context.Background()
 	res, sw, err := adpuProto.Send(ctx, 0xE0, 0x06, 0x00, 0x00, nil)
 	if err != nil {
-		panic(err)
+		logger.Error("unable to send ADPU protocol to device", "err", err)
 	}
 
 	logger.Info("SW", "sw", sw)
 	if sw != 0x9000 {
-		panic(fmt.Errorf("sw is not OK"))
+		logger.Error("SW is not OK")
+		return
 	}
 
 	logger.Info("Ledger ETH information", "arbitraryDataEnabled", res[0]&0x01, "erc20ProvisioningNecessary", res[0]&0x02, "starkEnabled", res[0]&0x04, "starkv2Supported", res[0]&0x08, "version", fmt.Sprintf("%d.%d.%d", res[1], res[2], res[3]))
